1e0e1b61a79d5a62debd7b598df81e7548a466cf
Maria Teofani Evernita Sagala <mariateovanies11@gmail.com>
2026-03-06 15:19:42 +0700

    update praktik 1A.py

--- praktik 1A.py
+++ praktik 1A.py
@@ -11,36 +11,14 @@ print(f"Data Pertama : {detak_jantung[0]} bpm\n")
 detak_jantung.append(75)
 
 
-# ITERATION
-
-# Mencetak semua data detak detak_jantung
-print("Daftar rentan detak jantung saat istirahat : ")
-for data in detak_jantung :
-    print(f"Detak jantung: {data} bpm")
-print()
-    
-
 #FUNCTION
 
-#Fungsi untuk mengecek detak jantung berdasarkan usia
-print("Data detak jantung normal : ")
-def cek(data):
-    if data == 81.6:
-        return "18-20 tahun"
-    elif data == 80.2:
-        return "21-30 tahun"
-    elif data == 78.5:
-        return "31-40 tahun"
-    elif data == 75.3:
-        return "41-50 tahun"
-    elif data == 73.9:
-        return "51-60 tahun"
+#Fungsi untuk menganalisa kondisi detak jantung
+def analisa_kondisi(bpm):
+    if bpm > 100:
+        return "Peringatan Takikardia (Detak Tinggi)"
     else:
-        return "60 tahun ke atas"
-        
-for data in detak_jantung:
-    usia = cek(data)
-    print(f"Detak Jantung {data} bpm -> Usia: {usia} ")
+        return "Kondisi Normal"
     
     
     
