290b5bcc6571f9188a3a66b0be8613f96b8afa86
Maria Teofani Evernita Sagala <mariateovanies11@gmail.com>
2026-03-06 14:53:43 +0700

    update praktik 1A.py

--- praktik 1A.py
+++ praktik 1A.py
@@ -4,16 +4,11 @@
 detak_jantung = [81.6, 80.2, 78.5, 75.3, 73.9]
 
 #Mengakses data berdasarkan index (dimulai dari 0)
-print(f"usia 18-20 tahun : {detak_jantung[0]} bpm")
-print(f"usia 21-30 tahun : {detak_jantung[1]} bpm")
-print(f"usia 31-40 tahun : {detak_jantung[2]} bpm")
-print(f"usia 41-50 tahun : {detak_jantung[3]} bpm")
-print(f"usia 51-60 tahun : {detak_jantung[4]} bpm")
+print(f"Data Pertama : {detak_jantung[0]} bpm")
+
 
 # Menambah data baru
 detak_jantung.append(72.8)
-
-print(f"usia < 60 tahun : {detak_jantung[5]} bpm")
 
 
 # ITERATION
@@ -27,20 +22,21 @@ for data in detak_jantung :
 #FUNCTION
 
 #Fungsi untuk mengecek detak jantung berdasarkan usia
-def cek(data):
-    if data >= 81 and data <= 82:
-        return("Usia 18-20 tahun")
-    elif data >= 79 and data <= 81:
-        return("Usia 21-30 tahun")
-    elif data >= 76 and data <= 79:
-        return("Usia 31-40 tahun")
-    elif data >= 74 and data <= 76:
-        return("Usia 41-50 tahun")
-    elif data >= 73 and data <= 74
-        return("Usia 51-60 tahun")
+def cek(index):
+    if index == 0:
+        return "18-20 tahun"
+    elif index == 1:
+        return "21-30 tahun"
+    elif index == 2:
+        return "31-40 tahun"
+    elif index == 3:
+        return "41-50 tahun"
+    elif index == 4:
+        return "51-60 tahun"
     else:
-        return("Usia 60 keatas / Lansia")
-    
+        return "60+ tahun"
+        
+        
 for data in detak_jantung:
     usia = cek(data)
     print(f"Detak Jantung {data} -> usia: {usia} ")
