0607a0072b08e443d66d9e3db84599ec9bf5295e
Maria Teofani Evernita Sagala <mariateovanies11@gmail.com>
2026-03-06 14:15:26 +0700

    update praktik 1A.py

--- praktik 1A.py
+++ praktik 1A.py
@@ -2,4 +2,6 @@
 detak_jantung = [81.6,80.2, 78.5, 75.3, 73.9]
 
 #Mengakses data berdasarkan index (dimulai dari 0)
-print(f" : {detak_jantung[0]} bpm")
+print(f"usia 18-20 tahun : {detak_jantung[0]} bpm")
+print(f"usia 21-30 tahun : {detak_jantung[1]} bpm")
+print(f"usia 31-40 tahun : {detak_jantung[2]} bpm")
