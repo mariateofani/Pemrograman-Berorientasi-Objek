a656ba9888bfd1bbc03c1ec5aa2d6fce9b53d936
Maria Teofani Evernita Sagala <mariateovanies11@gmail.com>
2026-03-06 14:13:02 +0700

    update praktik 1A.py

--- praktik 1A.py
+++ praktik 1A.py
@@ -1,5 +1,5 @@
 # Mendefinisikan list daya sistem (Sistem Monitoring Kesehatan Sederhana)
-detak_jantung = [10,20,45,30,25]
+detak_jantung = [81.6,80.2, 78.5,75.3 73.]
 
 #Mengakses data berdasarkan index (dimulai dari 0)
-print(f"Data Pertama : {detak_jantung[0]}%")
+print(f"Data Pertama : {detak_jantung[0]} bpm")
