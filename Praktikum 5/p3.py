e8b9b4f2389e965ca28bc9d54ecbe44de1347561
Maria Teofani Evernita Sagala <mariateovanies11@gmail.com>
2026-04-17 13:27:56 +0700

    update p3.py

--- Praktikum 5/p3.py
+++ Praktikum 5/p3.py
@@ -38,13 +38,10 @@ class BarangKonsumsi(Barang):
       
 class GudangPolimorfik:
    def __init__(self,file_db= 'gudang_v3.json'):
-    super().__init__(id_barang, nama, harga)
-    self.tgl_exp = tgl_exp
+       file
 
    def 
-    d.update({"tipe" : "konsumsi", "tgl_exp" :self.tgl_exp})
-      return 
-
+ 
 barang1 = BarangElektronik("B001", "Laptop", 15000000, 24)
 barang2 = BarangKonsumsi("B002", "Susu", 50000, "15-12-2026")
 
