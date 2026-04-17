7cc14c308b4e2efb18c4f48224e10371e9e27f82
Maria Teofani Evernita Sagala <mariateovanies11@gmail.com>
2026-04-17 13:32:07 +0700

    update p3.py

--- Praktikum 5/p3.py
+++ Praktikum 5/p3.py
@@ -42,7 +42,7 @@ class GudangPolimorfik:
        self koleksi = []
        self muat_data()
 
-   def 
+   def tambah_barang(self, objek_barang):
  
 barang1 = BarangElektronik("B001", "Laptop", 15000000, 24)
 barang2 = BarangKonsumsi("B002", "Susu", 50000, "15-12-2026")
