0f8c187ac35eb0ae230da7e12f5a17bbb676de14
Maria Teofani Evernita Sagala <mariateovanies11@gmail.com>
2026-04-17 10:47:51 +0700

    update p1.py

--- Praktikum 5/p1.py
+++ Praktikum 5/p1.py
@@ -1,10 +1,14 @@
-def kali (a,b):
+def kali (a, b):
     return a + b
     
-    print(tambah (2,3))
+    print(tambah (2, 3))
     print(tambah("Hello", "AI"))
    
     print("/n")
    
    
-def kali (c)
+def kali (c, d):
+    return c * d
+    
+    print(kali (2,3))
+    print(kali)
