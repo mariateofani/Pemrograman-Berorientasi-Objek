a390ee3067d6bb1ffb5c420d95f006227fc99408
Maria Teofani Evernita Sagala <mariateovanies11@gmail.com>
2026-06-05 14:56:04 +0700

    up

--- Praktikum 10/main.py
+++ Praktikum 10/main.py
@@ -1,32 +1,31 @@
-
 import tkinter as tk
 from tkinter import messagebox
 import json
 import os
 
-FILE_NAME = "users.json" # nama file buat nyimpen data user
+FILE_NAME = "users.json"
 
 # Fungsi untuk memuat data pengguna
 def load_users():
     if not os.path.exists(FILE_NAME):
         return {}
-    with open(FILE_NAME, "r") as f:
-        return json.load(f)
+
+    try:
+        with open(FILE_NAME, "r") as f:
+            return json.load(f)
+    except:
+        return {}
 
 # Fungsi untuk menyimpan data pengguna
 def save_users(users):
     with open(FILE_NAME, "w") as f:
-        json.dump(users, f, indent=4) # indent=4 biar file json nya rapi
+        json.dump(users, f, indent=4)
 
-# Fungsi Register
+    print("Data tersimpan:", users)
+
 def register():
     user = entry_user.get()
     pwd = entry_pwd.get()
-
-    if user == "" or pwd == "":
-        messagebox.showerror("Error", "Username dan Password tidak boleh kosong!")
-        return
-
     users = load_users()
 
     if user in users:
@@ -35,24 +34,15 @@ def register():
         users[user] = pwd
         save_users(users)
         messagebox.showinfo("Success", "Registrasi Berhasil!")
-        entry_user.delete(0, tk.END)
-        entry_pwd.delete(0, tk.END)
 
-# Fungsi Login
+# Logic Login
 def login():
     user = entry_user.get()
     pwd = entry_pwd.get()
-
-    if user == "" or pwd == "":
-        messagebox.showerror("Error", "Username dan Password tidak boleh kosong!")
-        return
-
     users = load_users()
 
     if users.get(user) == pwd:
         messagebox.showinfo("Login", "Login Berhasil! Selamat Datang.")
-        entry_user.delete(0, tk.END)
-        entry_pwd.delete(0, tk.END)
     else:
         messagebox.showerror("Login", "Username atau Password Salah!")
 
@@ -62,10 +52,12 @@ root.title("Sistem Informasi - Login/Register")
 root.geometry("300x250")
 
 tk.Label(root, text="Username:").pack(pady=5)
+
 entry_user = tk.Entry(root)
 entry_user.pack()
 
 tk.Label(root, text="Password:").pack(pady=5)
+
 # Parameter show="*" digunakan untuk menyamarkan ketikan password
 entry_pwd = tk.Entry(root, show="*")
 entry_pwd.pack()
@@ -74,14 +66,9 @@ btn_login = tk.Button(root, text="Login", command=login)
 btn_login.pack(pady=5)
 
 btn_reg = tk.Button(root, text="Register", command=register)
-btn_reg.pack(pady=5)
+btn_reg.pack()
 
 # Menjalankan perulangan utama agar jendela aplikasi tetap terbuka
 root.mainloop()
-
-
-
-
-
 
 
