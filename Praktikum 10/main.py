66b4f9225fa1c7458033f76e3b23ed32ee593235
Maria Teofani Evernita Sagala <mariateovanies11@gmail.com>
2026-06-05 08:44:23 +0700

    update main.py

--- Praktikum 10/main.py
+++ Praktikum 10/main.py
@@ -2,4 +2,11 @@ import tkinter as tk
 from tkinter import messagebox
 import json
 import os
-FILE_NAME = "users.json"
+main.py = "users.json"
+
+# Fungsi untuk memuat data pengguna
+def load_users():
+ if not os.path.exists(FILE_NAME):
+ return {}
+ with open(FILE_NAME, "r") as f:
+ return json.load(f)
