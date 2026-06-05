9c1d14036da56186ad38bdbdd4d7d807ea12271a
Maria Teofani Evernita Sagala <mariateovanies11@gmail.com>
2026-06-05 09:34:30 +0700

    update main.py

--- Praktikum 10/main.py
+++ Praktikum 10/main.py
@@ -11,7 +11,31 @@ def load_users():
  with open(FILE_NAME, "r") as f:
  return json.load(f)
  
- # Fungsi untuk menyimpan data pengguna
+# Fungsi untuk menyimpan data pengguna
 def save_users(users):
  with open(FILE_NAME, "w") as f:
  json.dump(users, f)
+
+def register():
+ user = entry_user.get()
+ pwd = entry_pwd.get()
+ users = load_users()
+ if user in users:
+ messagebox.showerror("Error", "Username sudah
+terdaftar!")
+ else:
+ users[user] = pwd
+ save_users(users)
+ messagebox.showinfo("Success", "Registrasi Berhasil!")
+ 
+ # Logic Login
+def login():
+ user = entry_user.get()
+ pwd = entry_pwd.get()
+ users = load_users()
+ if users.get(user) == pwd:
+ messagebox.showinfo("Login", "Login Berhasil! Selamat
+Datang.")
+ else:
+     messagebox.showerror("Login", "Username atau Password
+Salah!")
